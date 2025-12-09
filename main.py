3b56c0146dd55581382fbc62eb69cc8cd92e7531
hdh3-Himu <hdh3@student.le.ac.uk>
2025-12-09 22:42:54 +0000

    implimenting main.py - complete

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ from player import storage
 from player.playlist_manager import PlaylistManager
 from player.music_player import MusicPlayer
 
+
+def print_menu():
+    print("\n=== MUSIC PLAYER MENU ===")
+    print("1. Add Song")
+    print("2. Remove Song")
+    print("3. Search Songs")
+    print("4. List All Songs")
+    print("5. Play Song")
+    print("6. Pause")
+    print("7. Stop")
+    print("8. Exit")
+
+
+
