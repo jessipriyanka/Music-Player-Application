c73e2a8bf3c134644d1886c9f2c305b615ee5abe
posanijahnavi <posanijahnavi@gmail.com>
2025-12-26 18:00:51 +0000

    all execution role added

--- tests/symbolic/test_add_song_symbolic.py
+++ tests/symbolic/test_add_song_symbolic.py
@@ -11,4 +11,21 @@ def test_add_song_symbolic(tmp_path, monkeypatch):
 
     manager = PlaylistManager(storage)
 
+    try:
+        manager.add_song("", "Artist", 120)
+    except ValueError:
+        pass
+
+    try:
+        manager.add_song("Title", "", 120)
+    except ValueError:
+        pass
+
+    try:
+        manager.add_song("A", "B", -5)
+    except ValueError:
+        pass
+
+    song = manager.add_song("A", "B", 200)
+    assert song["id"] == 1
 
